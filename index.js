8d89a4e56cc1fbda3157a10e79b92dd369e9d226
Madina Abilnazarova <madina@MacBook-Air-Madina.local>
2023-04-03 21:50:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+
+// alert("Hello user!");
+
+// let userName = prompt('Как тебя зовут?', ' ')
+// console.log(userName);
+
+// let userAge = prompt('Сколько тебе лет?', ' ')
+// console.log(userAge);
+
+// let userGender = prompt('Укажите свой пол',' ')
+// console.log(userGender);
+
+// console.log(userName, userAge, userGender);
+
+// if(userAge>18){
+//     alert("Hello and Welcome")
+// }
+// else{
+//     alert("Hello , join the group for students")
+// }
+
+
+// let a = +prompt('a?', '');
+// console.log(a) // string
+// a = +a
+// console.log(a) // number
+
+// switch(a){
+//     case 0:
+//         alert("0");
+//         break;
+//     case 1:
+//         alert("1");
+//         break;
+//     case 2:
+//         alert("2,3");
+//         break;            
+//     case 3:
+//         alert("2,3");
+//         break;   
+// }
+
+
+let result=0;
+for( let i = 1; i<=100; i+=2){
+    result+=i;
+}
+console.log(result);
+
+
+// while(i=0 && i<3){
+//     alert(`number ${i}!`);  
+// }
